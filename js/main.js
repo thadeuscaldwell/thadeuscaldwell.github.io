f5bdf2af24dd2de2224d5ab32984b04bd9cf2780
Thadeus Caldwell <thadeusc1@gmail.com>
2019-03-19 18:28:56 -0400

    update

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Thadeus Caldwell.", "Designer.", "Developer."],
+      strings: ["Thadeus Caldwell.", "A Designer.", "A Developer."],
       typeSpeed: 100,
       loop: true,
     });
